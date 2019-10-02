42a66af9f90d78beb1547023d8543e5d2c5d4e38
AdriLP94 <adrianlopezpalmero@gmail.com>
2019-10-02 16:58:15 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React, {useState,useEffect,Fragment} from 'react';
+import React, {useState, useEffect, Fragment} from 'react';
+import Formulario from './components/Formulario';
+import axios from 'axios';
 
 
 function App() {
+
+  const [artista, guardarArtista] = useState('');
+  const [letra, guardarLetra] = useState([]);
+  const [info, guardarInfo] = useState({});
+
+  const consultarAPILetra = async busqueda =>{
+    const {artista, cancion} = busqueda;
+    const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+    const resultado = await axios (url);
+    guardarLetra(resultado.letra.lyrics);
+  }
+
+
   return (
-  <h1>Hola</h1>
+    <Fragment>
+      <Formulario 
+        consultarAPILetra = {consultarAPILetra} />
+    </Fragment>
   );
 }
 
